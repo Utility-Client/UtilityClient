32f5bf9320534d0ecd3fc2c7767b160bdc801f44
GamingCrafthd <samuel.mischl@gamingcraft.de>
2021-12-19 13:45:59 +0100

    Change version

--- src/main/java/org/utilityclient/UtilityClient.java
+++ src/main/java/org/utilityclient/UtilityClient.java
@@ -22,7 +22,7 @@ public class UtilityClient extends Thread {
     public static final CPSThread CPS_THREAD_INSTANCE = new CPSThread();
     public static final DiscordRP DISCORD_INSTANCE = new DiscordRP();
     private static final String CLIENT_NAME = "Utility Client";
-    private static final String CLIENT_VERSION = "2.11-DEV";
+    private static final String CLIENT_VERSION = "2.10.2-DEV";
     private static final UtilityClient CLIENT_INSTANCE = new UtilityClient();
     public static float fovModifier = 1.0f;
     public static ArrayList<KeyBinding> keyBinds = new ArrayList<>();
