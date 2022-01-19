a992543b8d0dcfd08c3f7b85acacff9ae6e3d817
GamingCrafthd <samuel.mischl@gamingcraft.de>
2022-01-19 22:13:07 +0100

    Add Gui Editor

--- src/main/java/org/utilityclient/UtilityClient.java
+++ src/main/java/org/utilityclient/UtilityClient.java
@@ -4,6 +4,7 @@ import net.minecraft.client.resources.I18n;
 import org.utilityclient.config.Config;
 import org.utilityclient.config.ConfigEntry;
 import org.utilityclient.crosshair.CrosshairManager;
+import org.utilityclient.debug.DebugScreen;
 import org.utilityclient.discord.DiscordRP;
 import org.utilityclient.macro.MacroManager;
 import org.utilityclient.overlay.ModuleHandler;
@@ -25,7 +26,7 @@ public class UtilityClient extends Thread {
     public static final CPSThread CPS_THREAD_INSTANCE = new CPSThread();
     public static final DiscordRP DISCORD_INSTANCE = new DiscordRP();
     private static final String CLIENT_NAME = "Utility Client";
-    private static final String CLIENT_VERSION = "2.13-PRE-3";
+    private static final String CLIENT_VERSION = "2.13-PRE-3-DEV";
     private static final UtilityClient CLIENT_INSTANCE = new UtilityClient();
     public static float fovModifier = 1.0f;
     public static ArrayList<KeyBinding> keyBinds = new ArrayList<>();
@@ -33,6 +34,7 @@ public class UtilityClient extends Thread {
     public static boolean renderOverlay = true;
     public static boolean isFulbrightEnabled = false;
     public static boolean streamerMode = false;
+    public static boolean debugMode = getVersion().endsWith("DEV");
 
     public static UtilityClient getInstance() {
         return CLIENT_INSTANCE;
@@ -71,7 +73,7 @@ public class UtilityClient extends Thread {
         addKeyBind(I18n.format("uc.keybinding.fulbright"), Config.getInteger(ConfigEntry.HOTKEY_FULBRIGHT), false);
         addKeyBind(I18n.format("uc.keybinding.overlay"), Config.getInteger(ConfigEntry.HOTKEY_OVERLAY), false);
         addKeyBind(I18n.format("uc.keybinding.copyCoords"), 66, false);
-        //addKeyBind("Debug hotkey", 67, false);
+        if(debugMode) addKeyBind("GuiScreen editor", 67, false);
 
         try {
             CrosshairManager.run();
@@ -103,14 +105,15 @@ public class UtilityClient extends Thread {
         if (keyBinds.size() >= 3) {
             if (keyBinds.get(0).isKeyDown()) fovModifier = Config.getFloat(ConfigEntry.ZOOM_FACTOR, 0.15f);
             else fovModifier = 1.0f;
+
             if (keyBinds.get(1).isPressed()) if (Minecraft.getMinecraft().gameSettings.gammaSetting == 1.0f) {
                 Minecraft.getMinecraft().gameSettings.gammaSetting = 999999;
                 isFulbrightEnabled = true;
-            }
-            else {
+            } else {
                 Minecraft.getMinecraft().gameSettings.gammaSetting = 1.0f;
                 isFulbrightEnabled = false;
             }
+
             if(keyBinds.get(2).isPressed()) renderOverlay = !renderOverlay;
 
             if(keyBinds.get(3).isPressed()) {
@@ -120,9 +123,7 @@ public class UtilityClient extends Thread {
                 clipboard.setContents(stringSelection, null);
             }
 
-            //if(keyBinds.get(4).isPressed()) {
-            //    Minecraft.getMinecraft().ingameGUI.setRecordPlaying("Running temporary test code.", false);
-            //}
+            if(debugMode) if(keyBinds.get(4).isPressed()) Minecraft.getMinecraft().displayGuiScreen(new DebugScreen());
         }
         MacroManager.loop();
     }
