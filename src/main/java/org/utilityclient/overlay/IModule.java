ce3f22a692ead06b8075ebcd4e523447f7325e1a
GamingCrafthd <samuel.mischl@gamingcraft.de>
2022-01-19 10:59:33 +0100

    Update IModule

--- src/main/java/org/utilityclient/overlay/IModule.java
+++ src/main/java/org/utilityclient/overlay/IModule.java
@@ -1,7 +1,26 @@
 package org.utilityclient.overlay;
 
 public interface IModule {
+    /**
+     * @return The name of the module, shown as prefix ingame.
+     */
     String getName();
+
+    /**
+     * @return The current value of the module. Shown as suffix.
+     */
     String getValue();
+
+    /**
+     * @return Your name, isn't shown anywhere yet.
+     */
     String getAuthor();
+
+    /**
+     * @return Should currently render?
+     * @implNote Used for modules, that might be disabled. Examples might be a Multiplayer-only module.
+     */
+    default boolean shouldRender() {
+        return true;
+    }
 }
