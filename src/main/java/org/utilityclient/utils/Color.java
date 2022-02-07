9b6745f8db200432eeae98435cdc269fae28048f
GamingCrafthd <samuel.mischl@gamingcraft.de>
2022-02-07 14:14:02 +0100

    FileUtils: Add Full JavaDocs
    Color: Add JavaDocs

--- src/main/java/org/utilityclient/utils/Color.java
+++ src/main/java/org/utilityclient/utils/Color.java
@@ -1,5 +1,11 @@
 package org.utilityclient.utils;
 
+/**
+ * Color codes used to draw UI in Minecraft.
+ * This enum was created to prevent the use of "magic numbers" and make it easier to render specific colors.
+ * @since 2.14
+ * @author GamingCraft
+ */
 public enum Color {
     TEXT(-1),                       // a:255 r:255 g:255 b:255
     BACKGROUND(1426063360),         // a:85 r:0 g:0 b:0
--- src/main/java/org/utilityclient/utils/FileUtils.java
+++ src/main/java/org/utilityclient/utils/FileUtils.java
@@ -7,7 +7,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Easy loading and saving of files.
+ */
 public class FileUtils {
+    /**
+     * @param file The file, that should be loaded
+     * @return A String array with all lines found in the file.
+     * @throws FileNotFoundException Gets thrown, if the file was not found.
+     */
     public static String[] loadFile(File file) throws FileNotFoundException {
         ArrayList<String> str = new ArrayList<>();
         Scanner s = new Scanner(file);
@@ -18,6 +26,11 @@ public class FileUtils {
         return str.toArray(new String[] {});
     }
 
+    /**
+     * @param file The file where the content should be written to.
+     * @param content The content, that should be written to the file. For new lines use "\n"
+     * @throws IOException If an I/O error occurs
+     */
     public static void saveFile(File file, String content) throws IOException {
         FileWriter fw = new FileWriter(file);
         fw.write(content);
