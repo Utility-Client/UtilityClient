4702edadd4614482a1880aba3a05ce4ef05f7309
GamingCrafthd <samuel.mischl@gamingcraft.de>
2022-02-07 14:25:50 +0100

    Update SerializationUtils.java
    More javadocs!

--- src/main/java/org/utilityclient/utils/SerializationUtils.java
+++ src/main/java/org/utilityclient/utils/SerializationUtils.java
@@ -9,6 +9,12 @@ import java.util.Base64;
  * @since 2.9.1
  */
 public class SerializationUtils {
+    /**
+     * Serializes a Serializable into a String, that can be stored or deserialized into an Object again.
+     * @param o The Serializable, that should be serialized.
+     * @return A string, that can be easier stored.
+     * @throws IOException If an I/O error occurs
+     */
     public static String serialize(Serializable o) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(baos);
@@ -17,6 +23,13 @@ public class SerializationUtils {
         return Base64.getEncoder().encodeToString(baos.toByteArray());
     }
 
+    /**
+     * Convert a string back into an object.
+     * @param s The input string
+     * @return An object
+     * @throws IOException If an I/O error occurs
+     * @throws ClassNotFoundException Class of a serialized object cannot be found.
+     */
     public static Object deserialize(String s) throws IOException,
             ClassNotFoundException {
         byte[] data = Base64.getDecoder().decode(s);
