349197dd64f47116029bf662c45f00100fad0071
GamingCrafthd <samuel.mischl@gamingcraft.de>
2022-02-13 23:35:05 +0100

    Utils: Add ignore(Object, boolean)
    + Utils: Add getSecondsOfDay()

--- src/main/java/org/utilityclient/utils/Utils.java
+++ src/main/java/org/utilityclient/utils/Utils.java
@@ -3,6 +3,10 @@ package org.utilityclient.utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
 /**
  * General Utils, that don't need a whole class.
  * @since 2.14
@@ -18,5 +22,26 @@ public class Utils {
      */
     public static void ignore(Object object) {
         logger.debug(object);
+    }
+
+    /**
+     * Logs object, if shouldLog is true. See {@link #ignore(Object)} for more details.
+     * @param object The return value, that isn't useful.
+     * @param shouldLog Should be the object logged or just fully ignored?
+     */
+    public static void ignore(Object object, boolean shouldLog) {
+        if(shouldLog) logger.debug(object);
+    }
+
+    /**
+     * Get seconds passed since midnight.
+     * @return Seconds passed since midnight as Long.
+     * @see ZonedDateTime
+     * @see Duration
+     * @see Instant
+     */
+    public static long getSecondsOfDay() {
+        ZonedDateTime nowZoned = ZonedDateTime.now();
+        return Duration.between(nowZoned.toLocalDate().atStartOfDay(nowZoned.getZone()).toInstant(), Instant.now()).getSeconds();
     }
 }
