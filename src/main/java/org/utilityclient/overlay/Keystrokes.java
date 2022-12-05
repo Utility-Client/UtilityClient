981ca988184cdcfd8c7a98a0e34db806106c0597
Samuel302 <samuel.mischl@gamingcraft.de>
2022-12-05 20:11:25 +0100

    Keystrokes: Fix wrong position data being used

--- src/main/java/org/utilityclient/overlay/Keystrokes.java
+++ src/main/java/org/utilityclient/overlay/Keystrokes.java
@@ -2,31 +2,31 @@ package org.utilityclient.overlay;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.Window;
 import org.utilityclient.config.Config;
 
 public class Keystrokes {
     public static void loop() {
         if(!Config.getBoolean("keystrokesEnabled", true)) return;
-        int width = MinecraftClient.getInstance().width;
-        int height = MinecraftClient.getInstance().height;
+        Window window = new Window(MinecraftClient.getInstance());
 
-        ButtonWidget a = new ButtonWidget(0, width-48, height-72, 20, 20, "W");
+        ButtonWidget a = new ButtonWidget(0, window.getWidth()-48, window.getHeight()-72, 20, 20, "W");
         a.active = MinecraftClient.getInstance().options.keyForward.isPressed();
         a.render(MinecraftClient.getInstance(), 0, 0);
 
-        ButtonWidget b = new ButtonWidget(0, width-72, height-48, 20, 20, "A");
+        ButtonWidget b = new ButtonWidget(0, window.getWidth()-72, window.getHeight()-48, 20, 20, "A");
         b.active = MinecraftClient.getInstance().options.keyLeft.isPressed();
         b.render(MinecraftClient.getInstance(), 0, 0);
 
-        ButtonWidget c = new ButtonWidget(0, width-48, height-48, 20, 20, "S");
+        ButtonWidget c = new ButtonWidget(0, window.getWidth()-48, window.getHeight()-48, 20, 20, "S");
         c.active = MinecraftClient.getInstance().options.keyBack.isPressed();
         c.render(MinecraftClient.getInstance(), 0, 0);
 
-        ButtonWidget d = new ButtonWidget(0, width-24, height-48, 20, 20, "D");
+        ButtonWidget d = new ButtonWidget(0, window.getWidth()-24, window.getHeight()-48, 20, 20, "D");
         d.active = MinecraftClient.getInstance().options.keyRight.isPressed();
         d.render(MinecraftClient.getInstance(), 0, 0);
 
-        ButtonWidget e = new ButtonWidget(0, width-72, height-24, 68, 20, "Space");
+        ButtonWidget e = new ButtonWidget(0, window.getWidth()-72, window.getHeight()-24, 68, 20, "Space");
         e.active = MinecraftClient.getInstance().options.keyJump.isPressed();
         e.render(MinecraftClient.getInstance(), 0, 0);
     }
